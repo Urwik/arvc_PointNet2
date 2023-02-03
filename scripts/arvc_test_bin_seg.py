4eb7052ac00d4e1fb24bcbfe63238f63b34a43d8
Urwik <f.soler@umh.es>
2023-02-03 10:35:43 +0100

    Update

--- scripts/arvc_test_bin_seg.py
+++ scripts/arvc_test_bin_seg.py
@@ -5,21 +5,21 @@ import torch
 import sklearn.metrics as metrics
 import sys
 import socket
+import yaml
 from datetime import datetime
 import warnings
 warnings.filterwarnings('ignore')
 
-machine_name = socket.gethostname()
-if machine_name == 'arvc-Desktop':
-    sys.path.append('/')
-else:
-    sys.path.append('/home/arvc/Fran/PycharmProjects/arvc_PointNet')
+# IMPORTS PATH TO THE PROJECT
+current_project_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+pycharm_projects_path = os.path.dirname(current_project_path)
+# IMPORTS PATH TO OTHER PYCHARM PROJECTS
+sys.path.append(current_project_path)
+sys.path.append(pycharm_projects_path)
 
-from model.arvc_PointNet_bin_seg_extraFeatures import PointNetDenseCls
-from arvc_utils.arvc_dataset import PLYDataset
-from arvc_utils.arvc_pointcloud_utils import np2ply
-
-saved_clouds = 0
+from arvc_Utils.Datasets import PLYDataset
+from arvc_Utils.pointcloudUtils import np2ply
+from models import arvc_pointnet2_bin_seg
 
 def test(device_, dataloader_, model_, loss_fn_):
     # TEST
@@ -43,16 +43,17 @@ def test(device_, dataloader_, model_, loss_fn_):
             rec_lst.append(avg_rec)
             conf_m_lst.append(conf_m)
 
-            save_pred_as_ply(data, pred_fix, OUT_CLOUDS_DIR, filename_)
+            if SAVE_PRED_CLOUDS:
+                save_pred_as_ply(data, pred_fix, PRED_CLOUDS_DIR, filename_)
 
             current_clouds += data.size(0)
 
-            if batch % 10 == 0 or data.size()[0] < dataloader_.batch_size:  # print every 10 batches
-                print(f'[Batch: {current_clouds}/{len(dataloader_.dataset)}],'
-                      f'  [Avg Loss: {avg_loss:.4f}] \n'
-                      f'   Avg F1 score: {avg_f1:.4f} \n'
-                      f'   Avg Precision score: {avg_pre:.4f} \n'
-                      f'   Avg Recall score: {avg_rec:.4f}')
+            if batch % 1 == 0 or data.size()[0] < dataloader_.batch_size:  # print every 10 batches
+                print(f'  [Batch: {current_clouds}/{len(dataloader_.dataset)}],'
+                      f'  [File: {str(filename_)}],'
+                      f'  [F1 score: {avg_f1:.4f}],'
+                      f'  [Precision score: {avg_pre:.4f}],'
+                      f'  [Recall score: {avg_rec:.4f}]')
 
     return loss_lst, f1_lst, pre_lst, rec_lst, conf_m_lst
 
@@ -103,62 +104,76 @@ def compute_metrics(label_, pred_):
 
 
 def save_pred_as_ply(data_, pred_fix_, output_dir_, filename_):
-    global saved_clouds
     data_ = data_.detach().cpu().numpy()
     batch_size = np.size(data_, 0)
     n_points = np.size(data_, 1)
 
-    date = datetime.today().strftime('%d.%m.%Y')
+    date = datetime.today().strftime('%y%m%d_%H%M')
     out_dir = os.path.join(output_dir_, date)
 
-    if not os.path.exists(output_dir_):
-        os.mkdir(output_dir_)
-
     if not os.path.exists(out_dir):
-        os.mkdir(out_dir)
+        os.makedirs(out_dir)
 
-    feat_xyzlabel = [('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('label', 'u4')]
+    feat_xyzlabel = [('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('nx', 'f4'), ('ny', 'f4'), ('nz', 'f4'), ('label', 'u4')]
 
     for i in range(batch_size):
-        xyz = data_[i][:, [0,1,2]]
+        xyz = data_[i][:, [0,1,2,3,4,5]]
         actual_pred = pred_fix_[i].reshape(n_points, 1)
         cloud = np.hstack((xyz, actual_pred))
         filename = filename_[0]
         np2ply(cloud, out_dir, filename, features=feat_xyzlabel, binary=True)
-        saved_clouds += 1
 
 
 if __name__ == '__main__':
+    start_time = datetime.now()
 
-    # HYPERPARAMETERS
-    BATCH_SIZE = 1
-    MAX_K = 1
-    THRESHOLD = 0.4651498919517908
+    # --------------------------------------------------------------------------------------------#
+    # GET CONFIGURATION PARAMETERS
+    CONFIG_FILE = 'test_configuration.yaml'
+    config_file_abs_path = os.path.join(current_project_path, 'config', CONFIG_FILE)
+    with open(config_file_abs_path) as file:
+        config = yaml.safe_load(file)
 
-    # PATHS
-    DATASET_DIR = os.path.abspath('/media/arvc/data/datasets/ARVC_GZF/test/ply_xyzlabelnormal')
-    BEST_MODEL_PATH = os.path.abspath('//model_save/bin_seg_xyzcurv/20.01.2023.09-16_50_0.001_ROC/best_model.pth')
-    OUT_CLOUDS_DIR = os.path.abspath('//pred_clouds/bin_seg_xyzcurv/roc')
-    saved_clouds = 0
+    # DATASET
+    TEST_DIR= config["TEST_DIR"]
+    FEATURES= config["FEATURES"]
+    LABELS= config["LABELS"]
+    NORMALIZE= config["NORMALIZE"]
+    BINARY= config["BINARY"]
+    # THRESHOLD_METHOS POSIBILITIES = cuda:X, cpu
+    DEVICE= config["DEVICE"]
+    BATCH_SIZE= config["BATCH_SIZE"]
+    # MODEL
+    MODEL_PATH= config["MODEL_PATH"]
+    # THRESHOLD= config["THRESHOLD"]
+    OUTPUT_CLASSES= config["OUTPUT_CLASSES"]
+    # LOSS = BCELoss()
+    # RESULTS
+    SAVE_PRED_CLOUDS= config["SAVE_PRED_CLOUDS"]
+    PRED_CLOUDS_DIR= config["PRED_CLOUDS_DIR"]
 
-    # SELECT DEVICE TO WORK WITH
-    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-    model = PointNetDenseCls(k = MAX_K, n_feat = 4).to(device)
-    loss_fn = torch.nn.BCELoss()
-
+    # --------------------------------------------------------------------------------------------#
     # INSTANCE DATASET
-    dataset = PLYDataset(root_dir = DATASET_DIR,
-                         features= [0, 1, 2, 7],
-                         labels = 3,
-                         normalize = True,
-                         binary = True,
+    dataset = PLYDataset(root_dir = TEST_DIR,
+                         features= FEATURES,
+                         labels = LABELS,
+                         normalize = NORMALIZE,
+                         binary = BINARY,
                          transform = None)
 
     # INSTANCE DATALOADER
     test_dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True, drop_last=False)
 
+    # SELECT DEVICE TO WORK WITH
+    device = torch.device(DEVICE)
+    model = arvc_pointnet2_bin_seg.get_model(num_classes=OUTPUT_CLASSES,
+                                             n_feat=len(FEATURES)).to(device)
+    loss_fn = torch.nn.BCELoss()
+
     # LOAD TRAINED MODEL
-    model.load_state_dict(torch.load(BEST_MODEL_PATH, map_location=device))
+    model.load_state_dict(torch.load(os.path.join(MODEL_PATH, 'best_model.pth'), map_location=device))
+    threshold = np.load(MODEL_PATH + f'/threshold.npy')
+    THRESHOLD = np.mean(threshold[-1])
 
     print('-'*50)
     print('TESTING ON: ', device)
@@ -170,9 +185,11 @@ if __name__ == '__main__':
     f1_score = np.mean(np.array(results[1]))
     precision = np.mean(np.array(results[2]))
     recall = np.mean(np.array(results[3]))
-
     confusion_matrix_list = np.array(results[4])
     conf_matrix = np.mean(confusion_matrix_list, axis=0)
+
+    print('\n\n')
+    print(f'Threshold: {THRESHOLD}')
     print(f'Avg F1_score: {f1_score}')
     print(f'Avg Precision: {precision}')
     print(f'Avg Recall: {recall}')
